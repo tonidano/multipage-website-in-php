0b279a9b4afb49fbac934ed046a1c67be813fbb1
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-07-30 11:32:42 +0200

    30/7 11h32 index.php

---
+++ header.php
@@ -0,0 +1,40 @@
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">Loupiote ASBL</a>
+  <button class="navbar-toggler" name="XX" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" value="XY" aria-label="hamburger">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" class="onmouseover()" href="#contact" target="_self" title="contact">Contact</a>
+      </li>
+    </ul>
+      </div>
+    </nav>
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+      <div class="collapse navbar-collapse nav-droite">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+    <ul class="navbar-nav nav-droite">
+      <li class="nav-item">
+        <a class="nav-link" class="onmouseover()" href="#articles" target="_self" title="articles">Articles</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" class="onmouseover()" href="#activités" target="_self" title="activités">Activités</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" class="onmouseover()" href="#cinéclubs" target="_self" title="ciné-clubs">Ciné-clubs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" class="onmouseover()" href="#outilspédagogiques" target="_self" title="outils pédagogiques">Outils pédagogiques</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>Loupiotes ASBL</h1>
+  </div>
+</div>
