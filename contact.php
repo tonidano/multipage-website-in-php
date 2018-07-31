4dd40cb9ba8fb013367fbbaec8c4ea3d0b499878
baptistefkt <baptistefirket@gmail.com>
2018-07-31 16:21:17 +0200

    contact update et formaulaire

--- contact.php
+++ contact.php
@@ -1,21 +1,26 @@
-<div class="container-fluid">
-    <div class="row aboutPic">
-        <div class="col-6">
-            <img src="./assets/images/equipeloupiot.png"  class="img-fluid" alt="">
+<div class="container-fluid aboutContainer">
+    <h1>Qui sommes nous ?</h1>
+    <div class="row aboutBigPic">
+        <div class="col-xs-12 col-md-12">
+            <img src="./assets/images/equipeloupiot.png"  class="img-rounded" alt="photo de l'équipe de loupiote">
         </div>
-        <div class="col-6">
-            <img src="./assets/images/cameraman.png" alt="">
+    </div>
+    <div class="row aboutPic">
+        <div class="col-xs-12 col-md-6">
+            <img src="./assets/images/cameraman.png" class="img-fluid" alt="photo homme tenant caméra">
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <img src="./assets/images/fille_clap.png" class="img-fluid" alt="photo fille faisant clap">
         </div>
     </div>
     <div class="row aboutText">
-        <div class="col">
-            <h1>Qui sommes nous ?</h1>
+        <div class="col-xs-12">
             <p>L’asbl Loupiote est composée d’une équipe de professionnels du cinéma et de pédagogues qui développent des ateliers itinérants d’éducation au et par le cinéma dans les milieux associatifs et scolaires.</p>
             <p>Loupiote est parrainée par les cinéastes Frédéric Fonteyne, Marion Hänsel, Bouli Lanners, Benoît Mariage, Pierre-Paul Renders et Jaco Van Dormael et aussi par Thomas d’Ansembourg (thérapeute et formateur en Communication Non Violente).</p>
             <p>L’association est reconnue comme Organisation de Jeunesse au sein de la Fédération Wallonie Bruxelles. Elle a pour mission de sensibiliser les jeunes à une lecture critique du langage cinématographique et d’aiguiser leur regard sur les médias qu’ils consomment quotidiennement. Ciné, TV, Dvd, Internet… Les images fascinent et prennent une place de plus en plus importante. En leur proposant des clefs de lecture et différents modes d’appropriation du média via leur participation active à divers projets cinématographiques, Loupiote encourage les jeunes à devenir des spectateurs actifs et critiques, prêts à faire entendre leur voix et à exercer leur rôle de citoyens responsables au sein de la société médiatique dans laquelle ils vivent.</p>
             <p>Dans son objectif de former des CRACS (des quoi? Citoyens Responsables Actifs Critiques et Solidaires!), l’ASBL Loupiote développe ainsi depuis plusieurs années, aux quatre coins de la Fédération Wallonie-Bruxelles, des projets innovants et plusieurs types d’activités relatifs à l’éducation des jeunes au et par le cinéma :</p>
         </div>
-        <div class="col">
+        <div class="col-xs-12">
             <ul>
                 <li>La création d’outils pédagogiques</li>
                 <li>Différents modules d’animation au départ de projections de films (« Les Enfants du Ciné », « Les Ados du Ciné », « Les Samedis du Ciné » et autres séances ciné-débat)</li>
@@ -25,59 +30,110 @@
             </ul>
         </div>
     </div>
-    <div class="row contact">
-        <div class="col">
-            <h1>Nous contacter</h1>
-            <form class="formulaire" action="" method="post">
-                <fieldset>
-                    <div class="form titre">
-                        <p>titre</p>
-                        <input id="madame" type="radio" name="titre" value="Mme">
-                        <label for="madame">Mme</label>
-                        <input id="mademoiselle" type="radio" name="titre" value="Melle">
-                        <label for="mademoiselle">Melle</label>
-                        <input id="monsieur" type="radio" name="titre" value="Mr.">
-                        <label for="monsieur">Mr.</label>
+</div>
+<div class="container-fluid formContainer">
+
+    <div class="row">
+
+        <div class="col-xs-12 col-md-8 offset-2">
+            <h1>Contactez-nous</h1>
+            <form method="post">
+                <fieldset class="form-group">
+                    <div class="row">
+                        <legend class="col-form-label col-sm-2 pt-0"><h6>titre</h6></legend>
+                        <div class="col-sm-10">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="titreRadios" id="madameForm" value="option1" checked>
+                                <label class="form-check-label" for="madameForm">
+                                Mme
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="titreRadios" id="melleForm" value="option2">
+                                <label class="form-check-label" for="melleForm">
+                                Melle
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline disabled">
+                                <input class="form-check-input" type="radio" name="titreRadios" id="mrForm" value="option3">
+                                <label class="form-check-label" for="mrForm">
+                                Mr.
+                                </label>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form form nom">
-                        <p>nom</p>
-                        <input type="text" name="nom" value="Nom">
-                    </div>
-                    <div class="form prenom">
-                        <p>prenom</p>
-                        <input type="text" name="prenom" value="Prénom">
-                    </div>
-                    <div class="form email">
-                        <p>email</p>
-                        <input type="email" name="email" value="email">
-                    </div>
-                    <div class="form objet">
-                        <p>objet</p>
-                        <select class="select" name="objet">
-                            <option value="info">Demande d'information</option>
-                            <option value="autre">Autre</option>
-                        </select>
-                    </div>
-                    <div class="form message">
-                        <p>Votre message</p>
-                        <input type="text" name="message" value="">
-                    </div>
-                    <div class="form documents">
-                        <p>documents</p>
-                        <input type="file" name="documents" value="">
-                    </div>
-                    <div class="form format">
-                        <p>format de réponse souhaité</p>
-                        <input id="html" type="radio" name="format" value="html">
-                        <label for="html">HTML</label>
-                        <input id="texte" type="radio" name="format" value="texte">
-                        <label for="texte">Texte</label>
-                    </div>
-                    <button type="submit" name="button">Contactez-moi</button>
                 </fieldset>
+                <div class="form-group row">
+                    <label for="nomForm" class="col-sm-2 col-form-label"><h6>nom</h6></label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="nomForm" placeholder="Nom">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="prenomForm" class="col-sm-2 col-form-label"><h6>prenom</h6></label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="prenomForm" placeholder="Prénom">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="emailForm" class="col-sm-2 col-form-label"><h6>email</h6> </label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="emailForm" placeholder="email">
+                    </div>
+                </div>
+                <fieldset class="form-group">
+                    <div class="row">
+                        <legend class="col-form-label col-sm-2 pt-0"><h6>objet</h6> </legend>
+                        <div class="col-sm-10">
+                            <div class="form-check form-check-inline">
+                                <select class="form-check-input" name="objetSelect">
+                                    <option selected>Choisissez...</option>
+                                    <option value="1">Demande d'informations</option>
+                                    <option value="2">Autre</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+                <div class="form-group row">
+                    <label for="messageForm" class="col-sm-2 col-form-label"><h6>Votre message</h6> </label>
+                    <div class="col-sm-10">
+                        <textarea class="form-control" id="messageForm" name="messageForm"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="documentForm" class="col-sm-2 col-form-label"><h6>document</h6> </label>
+                    <div class="col-sm-10">
+                        <input type="file" class="form-control" id="documentForm">
+                    </div>
+                </div>
+                <fieldset class="form-group">
+                    <div class="row">
+                        <legend class="col-form-label col-sm-2 pt-0"><h6>format de réponse souhaité</h6> </legend>
+                        <div class="col-sm-10">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="formatRadios" id="htmlForm" value="option1" checked>
+                                <label class="form-check-label" for="htmlForm">
+                                HTML
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="formatRadios" id="texteForm" value="option2">
+                                <label class="form-check-label" for="texteForm">
+                                Texte
+                                </label>
+                            </div>
+
+                        </div>
+                    </div>
+                </fieldset>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Contactez-moi</button>
+                    </div>
+                </div>
             </form>
         </div>
 
     </div>
-
 </div>
